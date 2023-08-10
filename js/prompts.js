231742a4905275845192f00b4f46948e0b0e9658
Kevin <accounts@kevinlwei.com>
2023-08-10 15:29:44 +0100

    Update prompts.js
    Corrected grammatical error

--- js/prompts.js
+++ js/prompts.js
@@ -15,7 +15,7 @@ var intro = `Right now, I'm a first-year at Harvard Law School. I'm also a train
 \t 
 \t To learn more about me, type <span class="special">social</span> to find me around the interwebs, <span class="special">portfolio</span> to see my past work, or <span class="special">contact</span> to reach out to me.`;
 
-var unformatted_intro = `Hey there! I'm <span class="special" >Kevin Wei</span>: a <span class="special">policy researcher</span>, <span class="special">technologist</span>, and <span class="special">effective altruist</span> based in Boston. I'm interested in issues (at the intersection of) tech policy & AI governance, economic + racial equity, and EA. Currently a first-year at Harvard Law. Feel free to grab some time on my <a href="https://calendly.com/kevinlwei" target="_blank">calendar</a> or drop me a message at <a href="mailto:hi@kevinlwei.com" target="_blank">hi@kevinlwei.com</a> :)
+var unformatted_intro = `Hey there! I'm <span class="special" >Kevin Wei</span>: a <span class="special">policy researcher</span>, <span class="special">technologist</span>, and <span class="special">effective altruist</span> based in Boston. I'm interested in issues (at the intersection) of tech policy & AI governance, economic + racial equity, and EA. Currently a first-year at Harvard Law. Feel free to grab some time on my <a href="https://calendly.com/kevinlwei" target="_blank">calendar</a> or drop me a message at <a href="mailto:hi@kevinlwei.com" target="_blank">hi@kevinlwei.com</a> :)
 \t
 Type <span class="special">hello</span> below to learn more about me, or try <span class="special">?</span> or <span class="special">help</span> to see more commands.`;
 
